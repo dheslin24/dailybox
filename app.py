5910d8637d6d749a0316f96a83fe1f81ef769852
dan heslin <dheslin@localhost.localdomain>
2020-11-24 20:31:51 -0800

    tracking logins

--- app.py
+++ app.py
@@ -1200,6 +1200,14 @@ def login():
 @login_required
 def index():
     grid = init_grid()
+    print("******************")
+    print("******************")
+    print("**                ")
+    print("**   {}           ".format(session["username"]))
+    print("** logged in      ")
+    print("**                ")
+    print("******************")
+    print("******************")
 
     return render_template("index.html", grid=grid, x=x_nums, y=y_nums)
 
