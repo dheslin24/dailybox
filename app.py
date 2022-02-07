581fb3a9e67fc4e09ed4c919d9974bfb6860f669
dan heslin <dheslin@yahoo.com>
2022-02-06 21:48:43 -0500

    more testing

--- app.py
+++ app.py
@@ -3445,7 +3445,7 @@ def send_bygemail():
     print(body)
 
     if userid == 19 or userid == '19':
-        send_email()
+        send_email(rcpt=rcpt, subj=subject, b_text=body)
 
     return redirect(url_for('email_users'))
 
