2503265d5218bd7cbdc06cd92cf30e18fd1bb193
dan heslin <dheslin@yahoo.com>
2023-01-11 23:28:21 -0500

    logging

--- check_start.py
+++ check_start.py
@@ -4,14 +4,19 @@ import subprocess
 # import signal
 # import os
 import time
+import logging
+
+logging.basicConfig(filename="check_start.log", format="%(asctime)s %(levelname)-8s %(message)s", level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
 
 
 flask_apps = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'flask' in p.info['name']]
 
 print(flask_apps)
+logging.info(flask_apps)
 
 if not flask_apps:
     print("starting flask")
+    logging.info("starting flask")
     #subprocess.run(['flask', 'run', '-h', '0.0.0.0', '-p', '10001', '%'], shell=True)
     subprocess.run(["flask run -h 0.0.0.0 -p 10001 &"], shell=True)
 
@@ -19,6 +24,7 @@ if not flask_apps:
 
     new_flask_app = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'flask' in p.info['name']]
     print("new flask app running with pid {}".format(new_flask_app))
+    logging.info("new flask app running with pid {}".format(new_flask_app))
 
 
 
