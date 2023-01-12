e96a40dd06fdff16f4ce5b03a5a6a6ed36b658f6
dan heslin <dheslin@yahoo.com>
2023-01-11 23:53:54 -0500

    restart at midnight

--- restart.py
+++ restart.py
@@ -25,7 +25,7 @@ for f in flask_apps:
 
 print("starting flask")
 #subprocess.run(['flask', 'run', '-h', '0.0.0.0', '-p', '10001', '%'], shell=True)
-subprocess.run(["flask run -h 0.0.0.0 -p 10001 &"], shell=True)
+subprocess.run(["flask run -h 0.0.0.0 -p 10001 &"], shell=True, cwd="/home/dheslin/dailybox")
 
 time.sleep(5)
 
