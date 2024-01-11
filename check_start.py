4a73048ec63a2203d01be6cccec9cfcef5a74b36
dan heslin <dheslin@yahoo.com>
2024-01-10 20:14:20 -0500

    backout check_start changes

--- check_start.py
+++ check_start.py
@@ -8,7 +8,7 @@ logging.basicConfig(filename="check_start.log", format="%(asctime)s %(levelname)
 
 
 flask_apps = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'flask' in p.info['name']]
-line_checker_app = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'line_checker' in p.info['name']]
+# line_checker_app = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'line_checker' in p.info['name']]
 
 week = 1
 season = 3 # 1 pre, 2 reg, 3 post
@@ -26,16 +26,16 @@ if not flask_apps:
     logging.info(f"new flask app running with pid {new_flask_app}")
 
 
-if not line_checker_app:
-    print("starting line_checker")
-    logging.info("starting line_checker")
+# if not line_checker_app:
+#     print("starting line_checker")
+#     logging.info("starting line_checker")
 
-    subprocess.run([f"python ./line_checker/line_checker.py {season} {week} {league}&"], shell=True, cwd="/home/dheslin/dailybox")
+#     subprocess.run([f"python ./line_checker/line_checker.py {season} {week} {league}&"], shell=True, cwd="/home/dheslin/dailybox")
 
-    time.sleep(5)
+#     time.sleep(5)
 
-    new_line_checker = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'line_checker' in p.info['name']]
-    print(f"new line checker running with pid {new_line_checker}")
-    logging.info(f"new line checker running with pid {new_line_checker}")
+#     new_line_checker = [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'line_checker' in p.info['name']]
+#     print(f"new line checker running with pid {new_line_checker}")
+#     logging.info(f"new line checker running with pid {new_line_checker}")
 
 
