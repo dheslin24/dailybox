4f907845f022ce5dce5556a1d456260d9160faa2
dan heslin <dheslin@yahoo.com>
2024-01-18 19:02:06 -0500

    handle no logo

--- app.py
+++ app.py
@@ -919,17 +919,21 @@ def display_box():
     for i in range(10):
         away_team[str(i)] = ''
     if len(away) == 3:
-        away_team['2'] = team_scores[away]['logo']
+        # away_team['2'] = team_scores[away]['logo']
+        away_team['2'] = team_scores[away].get('logo', "TBD")
         away_team['3'] = away[0]
         away_team['4'] = away[1]
         away_team['5'] = away[2]
-        away_team['6'] = team_scores[away]['logo']
+        # away_team['6'] = team_scores[away]['logo']
+        away_team['6'] = team_scores[away].get('logo', "TBD")
         print(f"LEN AWAY TEAM {len(away_team)}")
     else:
-        away_team['3'] = team_scores[away]['logo']
+        # away_team['3'] = team_scores[away]['logo']
+        away_team['3'] = team_scores[away].get('logo', "TBD")
         away_team['4'] = away[0]
         away_team['5'] = away[1]
-        away_team['6'] = team_scores[away]['logo']
+        # away_team['6'] = team_scores[away]['logo']
+        away_team['6'] = team_scores[away].get('logo', "TBD")
 
     print(f"paytype:  {pay_type}")
     # check for final scores only
