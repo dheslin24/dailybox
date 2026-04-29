d86da6c0d28fb7dd7c5a455cf6c0f08465a15a05
dan heslin <dheslin@yahoo.com>
2026-04-28 23:23:28 -0400

    remove old jinja routes

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,64 +1,11 @@
-from flask import Blueprint, flash, jsonify, redirect, render_template, request, session, url_for, Markup
+from flask import Blueprint, jsonify, redirect, request, session
 from db_accessor.db_accessor import db2
-from constants import PAY_TYPE_ID, BOX_TYPE_ID, EMOJIS, ALLOWED_EXTENSIONS, UPLOAD_FOLDER
-from utils import apology, login_required, admin_required
+from constants import BOX_TYPE_ID
+from utils import login_required, admin_required
 from email_helper import send_email
 import logging
 
 bp = Blueprint('admin', __name__)
-
-
-@bp.route("/create_alias", methods=["POST", "GET"])
-@login_required
-def create_alias():
-    r = request.form.get('alias_boxnum')
-    box_tuple = eval(r)
-    boxid = box_tuple[0]
-    boxnum = box_tuple[1]
-
-    user_dict = dict(db2("SELECT userid, username FROM users;"))
-
-    aliases_result = db2("SELECT userid FROM users WHERE alias_of_userid = %s and active = 1", (session['userid'],))
-    user_aliases = []
-    if aliases_result:
-        for alias in aliases_result:
-            user_aliases.append((user_dict[alias[0]], alias[0]))
-
-    return render_template("create_alias.html", boxid=boxid, boxnum=boxnum, user_aliases=user_aliases)
-
-
-@bp.route("/assign_alias", methods=["POST", "GET"])
-@login_required
-def assign_alias():
-    boxid = str(request.form.get('boxid'))
-    boxnum = str(int(request.form.get('boxnum')) - 1) #boxes displayed start at 1.  boxes in db start at 0.
-
-    user_aliases = eval(request.form.get('user_aliases'))
-    user_alias_dict = dict(user_aliases)
-
-    if request.form.get('existingAlias'):
-        existing_alias = eval(request.form.get('existingAlias'))
-    else:
-        existing_alias = None
-
-    new_alias = request.form.get('newAliasName')
-
-    if existing_alias:
-        db2("UPDATE boxes SET box%s = %s WHERE boxid = %s;", (int(boxnum), existing_alias[1], int(boxid)))
-
-    elif new_alias in user_alias_dict:
-        db2("UPDATE boxes SET box%s = %s WHERE boxid = %s;", (int(boxnum), user_alias_dict[new_alias], int(boxid)))
-
-    elif new_alias:
-        db2("INSERT INTO users (username, password, first_name, last_name, active, alias_of_userid) values (%s, 'x', 'alias', %s, 1, %s);",
-            (new_alias, session['username'], session['userid']))
-
-        new_userid = db2("SELECT userid FROM users WHERE username = %s;", (new_alias,))[0][0]
-        logging.info("new alias userid: %s", new_userid)
-
-        db2("UPDATE boxes SET box%s = %s WHERE boxid = %s;", (int(boxnum), int(new_userid), int(boxid)))
-
-    return redirect(url_for("boxes.my_games"))
 
 
 @bp.route("/api/create_alias", methods=["GET"])
@@ -99,15 +46,6 @@ def api_assign_alias():
     return jsonify({'success': True})
 
 
-@bp.route("/admin", methods=["GET", "POST"])
-def admin():
-    is_admin = db2("SELECT is_admin FROM users WHERE userid = %s;", (session['userid'],))
-    if is_admin and is_admin[0][0] == 1:
-        payout_type = db2("SELECT pay_type_id, description FROM pay_type;")
-        return render_template("admin.html", payout_type=payout_type)
-    else:
-        return apology("Sorry, you're not an admin")
-
 @bp.route("/api/admin", methods=["GET"])
 def api_admin():
     if not session.get('userid') or not db2("SELECT is_admin FROM users WHERE userid = %s;", (session['userid'],))[0][0]:
@@ -115,7 +53,7 @@ def api_admin():
     payout_types = db2("SELECT pay_type_id, description FROM pay_type;")
     return jsonify({'payout_types': [{'id': r[0], 'description': r[1]} for r in payout_types]})
 
-@bp.route("/create_privategame_code", methods=["GET", "POST"])
+@bp.route("/create_privategame_code", methods=["POST"])
 def create_privategame_code():
     boxid = request.form.get('boxid')
     passcode = request.form.get('passcode')
@@ -124,21 +62,7 @@ def create_privategame_code():
     db2("INSERT INTO privatepass (boxid, pswd, admin) VALUES (%s, %s, %s);", (boxid, passcode, admin_id))
     db2("UPDATE boxes SET box_type = %s WHERE boxid = %s;", (BOX_TYPE_ID['private'], boxid))
 
-    return render_template("landing_page.html")
-
-@bp.route("/bygzomo", methods=["GET", "POST"])
-@login_required
-@admin_required
-def bygzomo():
-    show_tables = db2("show tables;")
-
-    if request.method == "POST":
-        q = request.form.get('query')
-        result = db2(q)
-    else:
-        q = ''
-        result = ''
-    return render_template("bygzomo.html", result=result, q=q, show_tables=show_tables)
+    return redirect('/app/landing_page')
 
 
 @bp.route("/api/bygzomo", methods=["GET", "POST"])
@@ -159,7 +83,7 @@ def api_bygzomo():
         return jsonify({'result': [], 'error': str(e), 'query': q})
 
 
-@bp.route("/add_money", methods=["GET", "POST"])
+@bp.route("/add_money", methods=["POST"])
 def add_money():
     username = request.form.get('username')
     amount = request.form.get('amount')
@@ -169,9 +93,9 @@ def add_money():
     balance += int(amount)
     db2("UPDATE users SET balance = %s WHERE username = %s;", (balance, username))
 
-    return redirect(url_for("admin.admin_summary"))
+    return redirect('/app/admin_summary')
 
-@bp.route("/add_boxes_for_user", methods=["GET", "POST"])
+@bp.route("/add_boxes_for_user", methods=["POST"])
 def add_boxes_for_user():
     username = request.form.get('username')
     boxes = request.form.get('boxes')
@@ -190,50 +114,14 @@ def add_boxes_for_user():
     else:
         db2("INSERT INTO max_boxes(userid, max_boxes) VALUES (%s, %s);", (int(userid), int(boxes)))
 
-    return redirect(url_for("admin.admin_summary"))
+    return redirect('/app/admin_summary')
 
-@bp.route("/admin_summary", methods=["GET", "POST"])
+@bp.route("/admin_summary", methods=["POST"])
 def admin_summary():
-    if request.method == "POST":
-        amt = request.form.get('amt_paid')
-        userid = request.form.get('userid')
-        db2("UPDATE users SET amt_paid = %s WHERE userid = %s;", (amt, userid))
-
-    users = db2("SELECT userid, username, first_name, last_name, last_update, email, mobile, is_admin, alias_of_userid FROM users where active = 1;")
-
-    paid = dict(db2("SELECT userid, amt_paid FROM users;"))
-    for item in paid:
-        if paid[item] is None:
-            paid[item] = 0
-
-    box_list = ['box' + str(x) + ' ,' for x in range(100)]
-    box_string = ''
-    for _ in box_list:
-        box_string += _
-    box_string = box_string[:-2]
-    box = "SELECT fee, pay_type, {} FROM boxes WHERE active = 1 or boxid between 26 and 36;".format(box_string)
-    all_boxes = db2(box)
-    user_box_count = {}
-    user_fees = {}
-    for game in all_boxes:
-        fee = game[0]
-        pay_type = game[1]
-        if pay_type == 5 or pay_type == 6 or pay_type == 7:
-            fee = fee // 10  # these are 10-man pools
-        for box in game[2:]:
-            if box != 0 and box != 1:
-                if box in user_box_count.keys():
-                    user_box_count[box] += 1
-                    user_fees[box] += fee
-                else:
-                    user_box_count[box] = 1
-                    user_fees[box] = fee
-
-    mbd = dict(db2("SELECT * FROM max_boxes;"))
-    logging.debug("max_boxes_dict: %s", mbd)
-    total_max = sum(mbd.values())
-
-    return render_template("admin_summary.html", users=users, d=user_box_count, fees=user_fees, paid=paid, mbd=mbd, total_max=total_max)
+    amt = request.form.get('amt_paid')
+    userid = request.form.get('userid')
+    db2("UPDATE users SET amt_paid = %s WHERE userid = %s;", (amt, userid))
+    return redirect('/app/admin_summary')
 
 
 @bp.route("/api/admin_summary", methods=["GET"])
@@ -269,17 +157,7 @@ def api_admin_summary():
         'total_max': total_max,
     })
 
-@bp.route("/email_users", methods=["GET", "POST"])
-@login_required
-def email_users():
-    if request.args['message_response']:
-        message_response = request.args['message_response']
-    else:
-        message_response = ''
-
-    return render_template("email_users.html", message_response=message_response)
-
-@bp.route("/send_bygemail", methods=["GET", "POST"])
+@bp.route("/send_bygemail", methods=["POST"])
 @login_required
 def send_bygemail():
     userid = request.form.get('userid')
@@ -290,6 +168,6 @@ def send_bygemail():
     logging.info("sending email for userid: %s to %s", userid, rcpt)
 
     if userid == 19 or userid == '19':
-        message_response = send_email(rcpt=rcpt, subj=subject, b_text=body, body_header=subject)
+        send_email(rcpt=rcpt, subj=subject, b_text=body, body_header=subject)
 
-    return redirect(url_for('admin.email_users', message_response=message_response))
+    return redirect('/app/email_users')
--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,175 +1,16 @@
-from flask import Blueprint, flash, jsonify, redirect, render_template, request, session, url_for, Markup
+from flask import Blueprint, jsonify, redirect, request, session
 from db_accessor.db_accessor import db2
-from constants import PAY_TYPE_ID, BOX_TYPE_ID, EMOJIS, ALLOWED_EXTENSIONS, UPLOAD_FOLDER
-from utils import apology, login_required, admin_required
+from utils import apology, login_required
 from passlib.apps import custom_app_context as pwd_context
 from email_validator import validate_email, EmailNotValidError
 from email_helper import send_email
 import config
-import json
 import requests
 import logging
 
 bp = Blueprint('auth', __name__)
 
 
-# LOGIN routine
-@bp.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in."""
-
-    # forget any user_id
-    session.clear()
-
-    # if user reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username")
-
-        # ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password")
-
-        else:
-            username = request.form.get("username")
-        # query database for username
-        s = "SELECT username, password, userid, failed_login_count, is_admin FROM users WHERE username = %s and active = 1"
-        user = db2(s, (username,))
-        if len(user) != 0:
-            u = user[0][0]
-            p = user[0][1]
-            uid = user[0][2]
-            failures = user[0][3]
-            admin = user[0][4]
-
-            if failures > 9:
-                logging.info("%s - %s just keeps trying to login", u, uid)
-                return apology("{}x??  man... you really have no clue what your password is..  you're locked out now even if you get it right - good job.  talk to TW (or if you are JZ trying brute force.. just stop - thanks)".format(failures))
-
-            # ensure username exists and password is correct
-            if not pwd_context.verify(request.form.get("password"), p):
-                failures += 1
-                db2("UPDATE users SET failed_login_count = %s WHERE userid = %s;", (failures, uid))
-                logging.info("%s - %s just had failed login attempt %s", u, uid, failures)
-                if failures == 10:
-                    return apology("That's it - you're done.  No more.. talk to TW.. BYE")
-                else:
-                    return apology(Markup("<img src='https://y.yarn.co/a7d5df02-3cfb-4327-b2fe-d1bc2287187d_text.gif'/><br>Failed login attempt {} of 10. <br><br>You're a portly fellow.. a bit long in the waistband?  So what's your pleasure; is it the salty snacks you crave?<br>No, no, no, no... yours is a sweet-tooth.  Oh, you may stray, but you'll always return to your dark master:  the cocoa-bean!<br><br><br>Try BOSCO... or reach out to customer support (TW) to reset.".format(failures)))
-        else:
-            return apology("username does not exist")
-
-        # remember which user has logged in
-        session["userid"] = uid
-        session["username"] = u
-        session["is_admin"] = admin
-
-        # reset fail count to 0 - you made it!
-        db2("UPDATE users SET failed_login_count = 0 WHERE userid = %s;", (uid,))
-        session.permanent = True
-
-        # redirect user to home page
-        return redirect(url_for("auth.index"))
-
-    # else if user reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-@bp.route('/')
-def index():
-    return redirect('/app/landing_page')
-
-# new landing page - redirect btwn pickem & boxes
-@bp.route("/landing_page", methods=["GET", "POST"])
-@login_required
-def landing_page():
-    return render_template("landing_page.html")
-
-# REGISTER new user
-@bp.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user."""
-    secret = config.captchasecret
-    # if user reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        r = requests.post('https://hcaptcha.com/siteverify', data = {'secret' : secret, 'response' : request.form['h-captcha-response']})
-        google_response = json.loads(r.text)
-
-
-        # ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username")
-        # ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password")
-        #Makes sure the user ticked the captcha
-        elif google_response['success'] == False:
-            return apology("so - you're a robot then?  did you not see the click here I'm not a robot thing??")
-        # ensure password was confirmed
-        elif not request.form.get("password_confirm"):
-            return apology("must confirm password")
-
-        # ensure email was submitted
-        elif not request.form.get("email"):
-            return apology("must enter email")
-
-        # ensure email was submitted
-        elif not request.form.get("first_name"):
-            return apology("must enter first name")
-
-        # ensure email was submitted
-        elif not request.form.get("last_name"):
-            return apology("must enter last name")
-
-        # ensure email was submitted
-        elif not request.form.get("mobile"):
-            return apology("must enter mobile number")
-
-        email = request.form.get("email")
-        try:
-            # Validate.
-            valid = validate_email(email)
-
-            # Update with the normalized form.
-            email = valid.email
-
-        except EmailNotValidError as e:
-            # email is not valid, exception message is human-readable
-            return apology("must use valid email")
-
-        # encrypt password
-        if request.form.get("password") == request.form.get("password_confirm"):
-            hash = pwd_context.hash(request.form.get("password"))
-        else:
-            return apology("password confirmation does not match")
-
-        check_userid = db2("SELECT userid FROM users WHERE username = %s;", (request.form.get("username"),))
-
-        if len(check_userid) > 0:
-            return apology("username already exists.  reach out to customer support (aka TW) to have it reset.")
-
-        db2(
-            "INSERT INTO users(username, password, email, active, is_admin, first_name, last_name, mobile, failed_login_count) VALUES(%s, %s, %s, 1, 0, %s, %s, %s, 0);",
-            (request.form.get("username"), hash, request.form.get("email"), request.form.get("first_name"), request.form.get("last_name"), request.form.get("mobile"))
-        )
-
-        uid = db2("SELECT userid FROM users WHERE username = %s;", (request.form.get("username"),))[0][0]
-
-        # temporary - add money to new user for testing
-        db2("UPDATE users SET balance = 100000 WHERE userid = %s;", (uid,))
-
-        # remember which user has logged in
-        session["userid"] = uid
-        session["username"] = request.form.get("username")
-
-        logging.info("%s just registered", session["username"])
-
-        # redirect user to home page
-        return redirect(url_for("auth.index"))
-
-    else:
-        return render_template("register.html")
 
 @bp.route("/user_reset", methods=["GET", "POST"])
 def user_reset():
@@ -191,25 +32,9 @@ def user_reset():
 
     db2("UPDATE users SET password = %s, failed_login_count = 0 WHERE username = %s;", (hash, username))
 
-    return redirect(url_for("admin.admin_summary"))
+    return redirect('/app/admin_summary')
 
 
-@bp.route("/user_details", methods=["GET", "POST"])
-@login_required
-def user_details():
-    #    0         1       2     3     4      5      6       7         8        9        10         11         12        13       14         15
-    # (userid, username, pswd, first, last, email, mobile, balance, amt_paid, active, isadmin, ispickem, failedlogin, isbowl, aliasuserid, image)
-    user = db2("SELECT * FROM users WHERE userid = %s;", (session['userid'],))[0]
-    logging.debug("user_details for userid %s", session['userid'])
-    user_dict = {'username':user[1], 'first_name':user[3], 'last_name':user[4], 'email':user[5], 'mobile':user[6], 'balance':user[7]}
-    if user[15] != None:
-        user_dict['image'] = user[15]
-
-    # get user aliases
-    alias_dict = dict(db2("SELECT username, IFNULL(image, '') from users where alias_of_userid = %s;", (session['userid'],)))
-    userid_dict = dict(db2("SELECT username, userid FROM users WHERE alias_of_userid = %s", (session['userid'],)))
-
-    return render_template("user_details.html", user_dict = user_dict, alias_dict = alias_dict, userid_dict=userid_dict)
 
 @bp.route("/api/login", methods=["POST"])
 def api_login():
@@ -340,19 +165,19 @@ def reset_password():
             if password == pswd_confirm:
                 hash = pwd_context.hash(password)
                 db2("UPDATE users SET password = %s WHERE userid = %s;", (hash, session['userid']))
-                return redirect(url_for('auth.user_details'))
+                return redirect('/app/user_details')
             else:
                 return apology("password confirmation does not match")
         else:
             return apology("old password is incorrect")
     else:
-        return render_template("reset_password.html")
+        return redirect('/app/reset_password')
 
 @bp.route("/deactivate_user", methods=["GET", "POST"])
 def deactivate_user():
     userid = request.form.get('userid')
     db2("UPDATE users SET active = 0 WHERE userid = %s;", (userid,))
-    return redirect(url_for("admin.admin_summary"))
+    return redirect('/app/admin_summary')
 
 # LOGOUT Routine
 @bp.route("/logout")
@@ -361,4 +186,4 @@ def logout():
     session.clear()
 
     #redirect to login
-    return redirect(url_for("auth.login"))
+    return redirect('/app/login')
