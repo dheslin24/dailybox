e136267b3a1e9a7e43793cf1c1a20b1fe7b8be84
dan heslin <dheslin@yahoo.com>
2026-05-01 22:24:56 -0400

    update landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,45 +1,56 @@
 import Layout from '../components/Layout'
 
+const CARDS = [
+  {
+    label: 'Playoff Pickem',
+    href: '/app/view_all_picks',
+    img: 'https://www.wmse.org/wp-content/uploads/2017/12/ralph.gif',
+  },
+  {
+    label: 'Super Bowl Boxes',
+    href: '/app/custom_game_list',
+    img: 'https://c.tenor.com/LuMmZd2WpjcAAAAC/super-bowl-simpsons.gif',
+  },
+  {
+    label: 'Survivor Pools',
+    href: '/app/survivor_pool',
+    img: '/static/simpsons_survivor.png',
+  },
+  {
+    label: 'Kentucky Derby Pool',
+    href: '/app/horse_racing',
+    img: '/static/simpsons_horse.gif',
+  },
+  {
+    label: 'Register for Private Event',
+    href: '/app/private_pswd',
+    img: 'https://pbs.twimg.com/media/ECkwxxLX4AE_btl.jpg',
+    sub: '(password required)',
+  },
+  {
+    label: 'Access Existing Private Events',
+    href: '/app/private_game_list',
+    img: 'https://static.simpsonswiki.com/images/9/98/Flaming_Moe%27s_%28location%29.png',
+  },
+]
+
 export default function LandingPage() {
   return (
     <Layout>
-      <h1><p>Choose Your Game</p></h1>
-      <table align="center" cellPadding="10">
-        <tbody>
-          <tr>
-            <td className="landing" style={{textAlign:'center', cursor:'pointer'}} onClick={() => window.location.href = '/app/view_all_picks'}>
-              <img src="https://www.wmse.org/wp-content/uploads/2017/12/ralph.gif" style={{display:'block', width:'90%', height:'auto', margin:'auto'}} alt="" />
-              <br />PLAYOFF PICKEM
-            </td>
-            <td className="landing" style={{textAlign:'center', cursor:'pointer'}} onClick={() => window.location.href = '/app/custom_game_list'}>
-              <img src="https://c.tenor.com/LuMmZd2WpjcAAAAC/super-bowl-simpsons.gif" style={{display:'block', width:'120%', height:'60%'}} alt="" />
-              <br /><br /><br /><br />Click Here for Super Bowl Boxes
-            </td>
-          </tr>
-          <tr style={{background:'#fff'}}>
-            <td colSpan="2" style={{textAlign:'center', cursor:'pointer', padding:'24px 0', background:'#fff'}} onClick={() => window.location.href = '/app/survivor_pool'}>
-              <img src="/static/simpsons_survivor.png" alt="Survivor Pools" style={{display:'block', margin:'auto', width:'90%', maxWidth:'900px', height:'auto', borderRadius:'8px'}} />
-              <div style={{fontSize:'1.5em', fontWeight:'bold', marginTop:'12px'}}>Survivor Pools</div>
-            </td>
-          </tr>
-          <tr style={{background:'#fff'}}>
-            <td colSpan="2" style={{textAlign:'center', cursor:'pointer', padding:'24px 0', background:'#fff'}} onClick={() => window.location.href = '/app/horse_racing'}>
-              <img src="/static/simpsons_horse.gif" alt="Derby Pool" style={{display:'block', margin:'auto', width:'90%', maxWidth:'900px', height:'auto', borderRadius:'8px'}} />
-              <div style={{fontSize:'1.5em', fontWeight:'bold', marginTop:'12px'}}>Kentucky Derby Pool</div>
-            </td>
-          </tr>
-          <tr>
-            <td className="landing" style={{textAlign:'center', cursor:'pointer'}} onClick={() => window.location.href = '/app/private_pswd'}>
-              <img src="https://pbs.twimg.com/media/ECkwxxLX4AE_btl.jpg" style={{display:'block', width:'auto', height:'auto', margin:'auto'}} alt="" />
-              <br />Register for Private Event<br />(password required)
-            </td>
-            <td className="landing" style={{textAlign:'center', cursor:'pointer'}} onClick={() => window.location.href = '/app/private_game_list'}>
-              <img src="https://static.simpsonswiki.com/images/9/98/Flaming_Moe%27s_%28location%29.png" style={{display:'block', width:'auto', height:'70%', margin:'auto'}} alt="" />
-              <br />Access Your Existing Private Events
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <h1>Choose Your Game</h1>
+      <div className="landing-grid">
+        {CARDS.map(({ label, href, img, sub }) => (
+          <a key={href} href={href} className="landing-card">
+            <div className="landing-card-img">
+              <img src={img} alt={label} />
+            </div>
+            <div className="landing-card-label">
+              {label}
+              {sub && <div className="landing-card-sub">{sub}</div>}
+            </div>
+          </a>
+        ))}
+      </div>
     </Layout>
   )
 }
